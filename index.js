5e2b1efa943b6bdaf3e67a953e3bdb8777336a61
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-08-10 23:48:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hi");
+// git checkout -b library-part2
+
+("use strict");
+
+// alert("hey this is alert!")
+// const place = confirm("are you here?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you now?", "Type here...");
+// console.log(currentPlace);
+
+const channel = prompt("type a youtuber");
+console.log(`https://youtube.com/${channel}`);
